18703240d59a9dd63e318334911dc7ef64e73f95
Bryson LeBlanc <bwl0016@auburn.edu>
2026-03-05 12:24:22 -0600

    bandit and ollama try

--- src/nonmix.cpp
+++ src/nonmix.cpp
@@ -9,6 +9,7 @@
 // TEST CHANGES - bwl0016
 // TEST CHANGES 2 - bwl0016
 // Test changes 3 - bwl0016
+// TEST CHANGES 4 with bandit and ollama - bwl0016
 
 Rcpp::List normalApprox(Rcpp::List XList, Rcpp::List YList, Rcpp::List ZList, Eigen::MatrixXd& W,
                         Rcpp::List mdata, Rcpp::List mdataSList,
